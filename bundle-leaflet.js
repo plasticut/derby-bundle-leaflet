4a52ebee179780079da046af6929632740881a85
plasticut <plasticut@gmail.com>
2014-06-01 22:28:10 +0400

    loadStatic

--- bundle-leaflet.js
+++ bundle-leaflet.js
@@ -4,3 +4,9 @@ derby.on('store', function(store) {
         browserify.add(__dirname + '/bower_components/leaflet/dist/leaflet.js');
     });
 });
+
+module.exports = function(app, options) {
+    if (app.loadStatic) {
+        app.loadStatic(__dirname + '/bower_components/leaflet/dist/');
+    }
+};
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 module.exports = function(app, options) {
     var derby = require('derby');
-    derby.util.serverRequire(module, './bundle-leaflet');
     app.loadStyles(__dirname + '/bower_components/leaflet/dist/leaflet.css');
+    var bundle = derby.util.serverRequire(module, './bundle-leaflet');
+    if (bundle) {
+        bundle(app, options);
+    }
 };
